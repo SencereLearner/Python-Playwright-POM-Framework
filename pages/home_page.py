69b7c8bf9a415fb2bf9a896c6f749aa0a313b0ef
Quick_Learner <genesisprognosis1156@gmai.com>
2025-08-10 17:44:09 -0400

    added another test

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,5 +1,7 @@
+import random
+
 import allure
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 from pages.base_page import BasePage
 
 
@@ -10,7 +12,11 @@ class HomePage(BasePage):
         super().__init__(page)
         self.home_page_url = 'https://www.insureon.com/'
         self.log_in_ele = self.page.get_by_role('link', name='Log In')
+        self.quotes_button_ele = self.page.locator('[data-test-id="navButtonCTA"]')
+        self.warn_message = self.page.locator(".message_tauXM > strong")
 
+    business_type_input_field = "//*[text()='Search for your business type']//following-sibling::div/input"
+    business_classes_to_check = ["Non-Existing One", "Fake One", "Bogus One", "Deceptive One"]
 
     @allure.step("Navigating to home page")
     def navigate_to_home_page(self):
@@ -18,6 +24,23 @@ class HomePage(BasePage):
 
     @allure.step("Clicking log in link")
     def click_log_in_link(self):
-        return self.log_in_ele.click()
+        self.log_in_ele.click()
+
+    @allure.step("Clicking Get Quotes button")
+    def click_get_quotes_button(self):
+        print("Quotes button count:", self.quotes_button_ele.count())
+        print("Quotes button visible:", self.quotes_button_ele.is_visible())
+        self.quotes_button_ele.click()
+
+    @allure.step("Open Get Quotes menu and enter randomly selected bus class")
+    def enter_bus_class(self):
+        selected_bus_class = random.choice(self.business_classes_to_check)
+        print("Selected Business Class: ", selected_bus_class)
+        self.wait_for_element_to_load(self.business_type_input_field).fill(selected_bus_class)
+
+    def verify_warn_message(self):
+        expect(self.warn_message).to_contain_text("Sorry, we couldn’t find")
+
+
 
 
--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,3 +1,5 @@
+import time
+
 import allure
 import pytest
 from playwright.sync_api import expect
@@ -13,3 +15,13 @@ def test_check_contact_us_text(contact_us_page, base_page, home_page):
     base_page.hover_over_header_menu_element("About Insureon")
     base_page.select_insurance_type("Contact Insureon")
     expect(contact_us_page.contact_us_ele).to_contain_text("For calls and emails, expect a response within 24 to 48 hours.")
+
+
+@BasePage.log_time
+@allure.title("Verify Business Class is present")
+@pytest.mark.regression
+def test_bus_class_search(home_page):
+    home_page.navigate_to_home_page()
+    home_page.click_get_quotes_button()
+    home_page.enter_bus_class()
+    home_page.verify_warn_message()
