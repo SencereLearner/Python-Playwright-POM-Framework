b7e01b4b5c8d14a63ed3239f8e2dde67e298f6a3
Quick_Learner <genesisprognosis1156@gmai.com>
2025-08-13 18:16:32 -0400

    improvements

--- conftest.py
+++ conftest.py
@@ -26,3 +26,10 @@ def contact_us_page(page):
 # to run with allure: 1)rm -rf allure-results 2) pytest --alluredir=allure-results 3) allure serve allure-results
 # for cross browser testing: pytest --browser webkit --browser firefox --browser chromium (will run in 3 browsers 1 by 1)
 # wait for specific element: myEle.wait_for(state='visible', timeout=5000)
+
+# For parallel test running:
+# pip install pytest-xdist
+# run from terminal:
+# pytest -v -s -n auto or pytest -vsn auto (it will use every core in the machine)
+# pytest -vsn 5 (it will use the indicated amount of cores in the machine, in this case 5). Safer option to avoid overload
+# it will collect all the pytest tests (all that start with word test) and run them in parallel based on computing power
